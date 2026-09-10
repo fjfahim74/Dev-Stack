11155c38406728f8245052b107e425d6ddf47816
FJ Fahim <fjfahim8@gmail.com>
2026-09-10 22:34:03 +0600

    Initialized React with Tailwind and DaisyUI

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1 className="text-6xl font-bold bg-amber-800">Dev Stack</h1>
+    </div>
+  )
+}
+
+export default App
